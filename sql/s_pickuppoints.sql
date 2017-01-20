dae941cdfd40536b55c46561260aeb5974f9cd0c
las1987 <las_home@mail.ru>
2017-01-20 15:49:03 +0300

    Add files via upload

--- sql/s_pickuppoints.sql
+++ sql/s_pickuppoints.sql
@@ -1,39 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Структура таблицы 's_pickupoints'
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2017 at 12:12 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simpla`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `s_pickuppoints`
 --
 DROP TABLE IF EXISTS `s_pickuppoints`;
+
 CREATE TABLE `s_pickuppoints` (
   `id` int(20) NOT NULL,
-  `body` varchar(255),
-  `pickuppoint_url` varchar(255),
+  `body` longtext,
+  `pickuppoint_url` varchar(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `metro_station` varchar(255),
+  `metro_station` varchar(255) DEFAULT NULL,
   `address` varchar(255) NOT NULL,
-  `latitude` varchar(255),
-  `longitude` varchar(255),
-  `photo` varchar(255),
+  `latitude` varchar(255) DEFAULT NULL,
+  `longitude` varchar(255) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
   `url` varchar(255) NOT NULL,
+  `web_site` varchar(255) NOT NULL,
   `worktime` varchar(255) NOT NULL,
-  `phone` varchar(255),
-  `summ_limit` decimal(10,2) DEFAULT '0',
+  `phone` varchar(255) DEFAULT NULL,
+  `summ_limit` decimal(10,2) DEFAULT '0.00',
   `weight_limit` int(6) DEFAULT '0',
   `sides_limit` int(6) DEFAULT '0',
-  `dimensions_limit` decimal(6,3) DEFAULT '0',
-  `size_limit` int(6) DEFAULT NULL DEFAULT '0',
+  `dimensions_limit` decimal(6,3) DEFAULT '0.000',
+  `size_limit` int(6) DEFAULT '0',
   `enabled` int(1) NOT NULL DEFAULT '0'
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
 --
--- Индексы таблицы `s_pickupoints`
+-- Dumping data for table `s_pickuppoints`
+--
+
+INSERT INTO `s_pickuppoints` (`id`, `body`, `pickuppoint_url`, `name`, `metro_station`, `address`, `latitude`, `longitude`, `photo`, `url`, `web_site`, `worktime`, `phone`, `summ_limit`, `weight_limit`, `sides_limit`, `dimensions_limit`, `size_limit`, `enabled`) VALUES
+(2, '', NULL, 'ПУНКТ ВЫДАЧИ ИНТЕРНЕТ ЗАКАЗОВ М.МОСКОВСКАЯ', 'Московская', '196066, Санкт-Петербург, Алтайская улица, 16 ', '59.8497', '30.327115', NULL, 'punkt-vydachi-internet-zakazov-mmoskovskaya', '', 'пн-вс с 10-22', '8 (812) 407-20-24', '0.00', 10, 150, '0.125', 0, 1),
+(3, '', NULL, 'ПУНКТ ВЫДАЧИ ИНТЕРНЕТ ЗАКАЗОВ М.ПИОНЕРСКАЯ', 'Пионерская', '197438, Санкт-Петербург, Коломяжский проспект, 15к2 ', '60.001109', '30.299734', NULL, 'punkt-vydachi-internet-zakazov-mpionerskaya', '', 'пн-вс 10-21', '8 (812) 407-36-20', '0.00', 10, 150, '0.125', 0, 1),
+(4, '', NULL, 'ПУНКТ ВЫДАЧИ ИНТЕРНЕТ ЗАКАЗОВ М.ОЗЕРКИ', 'Озерки', '194354, Санкт-Петербург, проспект Энгельса, 113к1 ', '60.037836', '30.325633', NULL, 'punkt-vydachi-internet-zakazov-mozerki', '', 'пн-вс 10-20', '8 (812) 407-36-20', '0.00', 20, 200, '0.296', 100, 1),
+(5, '', NULL, 'ПУНКТ ВЫДАЧИ ИНТЕРНЕТ ЗАКАЗОВ DPD 1', 'Всеволожск', 'г. Всеволожск, улица Плоткина, 21к1 ', '60.024239', '30.642837', NULL, 'punkt-vydachi-internet-zakazov-dpd-1', '', 'пн-вс 10-21', '8 (812) 407-20-24', '0.00', 7, 180, '0.216', 60, 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `s_pickuppoints`
 --
 ALTER TABLE `s_pickuppoints`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `s_pickupoints`
+-- AUTO_INCREMENT for table `s_pickuppoints`
 --
 ALTER TABLE `s_pickuppoints`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- sql/s_pickuppoints_options.sql
+++ sql/s_pickuppoints_options.sql
@@ -1,26 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Структура таблицы 's_pickupoints_options'
+-- Host: 127.0.0.1
+-- Generation Time: Jan 20, 2017 at 12:19 PM
+-- Server version: 10.1.19-MariaDB
+-- PHP Version: 5.6.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `simpla`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `s_pickuppoints_options`
+--
+--
+-- Структура таблицы 's_pickupoints'
 --
 DROP TABLE IF EXISTS `s_pickuppoints_options`;
+
 CREATE TABLE `s_pickuppoints_options` (
   `id` int(20) NOT NULL,
   `pickuppoint_id` int(11) NOT NULL,
-  `summ_min_value` decimal(10,2),
-  `summ_max_value` decimal(10,2),
+  `summ_min_value` decimal(10,2) DEFAULT NULL,
+  `summ_max_value` decimal(10,2) DEFAULT NULL,
   `pickup_price_value` decimal(6,2) DEFAULT NULL
-  
-  
-) ENGINE=MyISAM AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 --
--- Индексы таблицы `s_pickupoints_options`
+-- Dumping data for table `s_pickuppoints_options`
+--
+
+INSERT INTO `s_pickuppoints_options` (`id`, `pickuppoint_id`, `summ_min_value`, `summ_max_value`, `pickup_price_value`) VALUES
+(6, 5, '0.00', '1499.00', '300.00'),
+(1, 2, '0.00', '1499.00', '200.00'),
+(7, 5, '1500.00', '3000.00', '250.00'),
+(8, 4, '0.00', '1499.00', '150.00');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `s_pickuppoints_options`
 --
 ALTER TABLE `s_pickuppoints_options`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id`, `pickuppoint_id`),
   ADD KEY `pickuppoint_id` (`pickuppoint_id`);
 
-  --
--- AUTO_INCREMENT для таблицы `s_pickupoints_options`
 --
- ALTER TABLE `s_pickuppoints_options`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=0;
+-- AUTO_INCREMENT for dumped tables
+--
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
